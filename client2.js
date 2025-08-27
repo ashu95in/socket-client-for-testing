d6178ada59b2e477f11a659f746db40578e862dc
ashu <citoashu@gmail.com>
2025-08-27 21:29:23 +0530

    first commit

---
+++ client2.js
@@ -0,0 +1,21 @@
+const { io } = require("socket.io-client");
+
+// Connect to your Socket.IO server
+const socket = io("http://localhost:3000", {
+  transports: ["websocket"], // force websocket for testing
+});
+
+socket.on("connect", () => {
+  console.log("Connected to server with ID:", socket.id);
+   socket.emit('register','kirti')
+});
+  
+// socket.emit("private-message",{touserId:'kirti',message:'baby hain baby'});
+
+socket.on('rece-private-message',(data)=>{
+  console.log(data)
+  
+})
+socket.on("disconnect", () => {
+  console.log("Disconnected from server");
+});
---
+++ index.js
@@ -0,0 +1,26 @@
+const { io } = require("socket.io-client");
+
+// Connect to your Socket.IO server
+const socket = io("http://localhost:3000", {
+  transports: ["websocket"], // force websocket for testing
+});
+
+socket.on("connect", () => {
+  console.log("Connected to server with ID:", socket.id);
+   socket.emit('register','prisha')
+});
+  
+socket.emit("bookReq",{toUser:"kriti",title:'c++',course:"BCA",sem:2,author:'Ashu shrivastav',dateReq:'10/12/25',price:150});
+
+// socket.emit("send-private-message",{toUserId:'kirti',message:'baby hain baby'});
+
+// socket.on('rece-private-message',(data)=>{
+//   console.log(data)
+  
+// })
+// socket.emit("send_message",'hello world i from clicent sie');
+
+
+socket.on("disconnect", () => {
+  console.log("Disconnected from server");
+});
