896dedff8b0c8397e0e9ed1ae45844c3c558e409
ashu <ashutosh.95shrivastav@gmail.com>
2025-08-29 10:02:31 +0530

    added listner

--- client2.js
+++ client2.js
@@ -5,12 +5,14 @@ const socket = io("http://localhost:3000", {
   transports: ["websocket"], // force websocket for testing
 });
 
+
 socket.on("connect", () => {
   console.log("Connected to server with ID:", socket.id);
-   socket.emit('register','kirti')
+   socket.emit('register',{userId:'kriti',page:'Home'})
 });
   
-// socket.emit("private-message",{touserId:'kirti',message:'baby hain baby'});
+
+socket.emit("private-message",{touserId:'kirti',message:'baby hain baby'});
 
 socket.on('rece-private-message',(data)=>{
   console.log(data)
--- index.js
+++ index.js
@@ -7,9 +7,11 @@ const socket = io("http://localhost:3000", {
 
 socket.on("connect", () => {
   console.log("Connected to server with ID:", socket.id);
-   socket.emit('register','prisha')
+   socket.emit('register',{userId:'prisha',page:'Home'})
 });
   
+
+
 socket.emit("bookReq",{toUser:"kriti",title:'c++',course:"BCA",sem:2,author:'Ashu shrivastav',dateReq:'10/12/25',price:150});
 
 // socket.emit("send-private-message",{toUserId:'kirti',message:'baby hain baby'});
